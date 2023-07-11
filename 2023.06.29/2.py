f6ca8c2105403ca1129c066932cddb0e3c909e51
Sizov <ser-reb@yandex.ru>
2023-07-11 09:24:58 +0400

    Загрузка решения задачи №2

---
+++ 2023.06.29/2.py
@@ -0,0 +1,26 @@
+from collections.abc import Iterable
+
+def tree_leaves(leaves : list) -> int:
+    """Считает количество листьев на дереве."""
+    result = 0
+    
+    for elem in leaves:
+        if elem == 'leaf':
+            result += 1
+        else:
+            result += tree_leaves(elem)
+    
+    return result
+    
+    
+# >>> tree = [[[['leaf', 'leaf', 'leaf', 'leaf'], 'leaf', 'leaf', 'leaf'], [['leaf', 'leaf'], 'leaf', 'leaf'], ['leaf', 'leaf', 'leaf']], [['leaf', 'leaf'], ['leaf', 'leaf', 'leaf', 'leaf', 'leaf', 'leaf'], 'leaf', 'leaf', 'leaf'], [['leaf'], ['leaf', 'leaf', ['leaf', 'leaf', 'leaf']], 'leaf', 'leaf'], ['leaf', 'leaf', ['leaf', 'leaf'], 'leaf']]
+# >>>
+# >>> tree_leaves(tree)
+# 38
+# >>> 
+# >>> tree_leaves(['leaf',[]])
+# 1
+# >>>
+# >>> tree_leaves([])
+# 0
+# >>>  
