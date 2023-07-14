3a50003537ea4928d8d627a99bb0faee8fff4ad8
Gennadiy <renardes@gmail.com>
2023-07-14 19:47:47 +0500

    правки удобства чтения кода

--- 2023.06.29/1.py
+++ 2023.06.29/1.py
@@ -3,15 +3,15 @@ from collections.abc import Iterable
 
 def product(numbers: Iterable[float]) -> float:
     """Возвращает произведение чисел."""
-
     result = numbers[0]
     if len(numbers) > 1:
-          result *= product(numbers[1:])
-          
+        result *= product(numbers[1:])
     return float(result)
-    
+
+
 # >>> product(range(10, 60, 10))
 # 12000000.0
 # >>> product((0.12, 0.05, -0.09, 0.0, 0.21))
 # -0.0
-# >>> 
+# >>>
+
