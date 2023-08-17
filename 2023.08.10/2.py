9f9f7deb20db64db1f44f51d7d7ee3ca4263f939
Gennadiy <renardes@gmail.com>
2023-08-17 19:16:37 +0500

    правки удобства чтения кода

--- 2023.08.10/2.py
+++ 2023.08.10/2.py
@@ -4,12 +4,13 @@ from datetime import date
 from datetime import datetime as dt
 from numbers import Number
 
+
 class PowerMeter:
-    """ Описывает двухтарифный счётчик потреблённой электрической мощности."""
-    
-    # конструктор
-    def __init__(self) -> None:
-        # атрибуты экземпляра
+    """
+    Описывает двухтарифный счётчик потреблённой электрической мощности.
+    """
+    # ИСПОЛЬЗОВАТЬ: в большинстве случаев аннотацию возвращаемых значений встроенных методов можно опустить
+    def __init__(self):
         self.tariff1: decimal.Decimal = 5.28
         self.tariff2: decimal.Decimal = 2.5
         self.tariff2_starts: datetime.time = time(23,00)
@@ -25,10 +26,10 @@ class PowerMeter:
         if today.month == 12:
             return f'({today:%B}) {self.charges[date(today.year+1, 1, 1)]}'  
         else:
-            return f'({today:%B}) {self.charges[date(today.year, today.month + 1, 1)]}'
+            return f'({today:%B}) {self.charges[date(today.year, today.month+1, 1)]}'
         
     def meter(self, power: Number) -> Decimal:    
-        """ Вычисляет и возвращает стоимость согласно тарифному плану, действующему в текущий момент"""
+        """Вычисляет и возвращает стоимость согласно тарифному плану, действующему в текущий момент."""
         current_date_time = dt.now()
         current_time = current_date_time.time()
 
@@ -39,28 +40,29 @@ class PowerMeter:
             value = round(Decimal(power*self.tariff2), 2)
         
         if current_date_time.month == 12:
-            if date(current_date_time.year+1, 1, 1) in self.charges:                
+            if date(current_date_time.year+1, 1, 1) in self.charges:
                 self.charges[date(current_date_time.year+1, 1, 1)] += value
             else:
                 self.charges[date(current_date_time.year+1, 1, 1)] = value
         else:
-            if date(current_date_time.year+1, 1, 1) in self.charges: 
-                self.charges[date(current_date_time.year, current_date_time.month + 1, 1)] += value
+            if date(current_date_time.year+1, 1, 1) in self.charges:
+                self.charges[date(current_date_time.year, current_date_time.month+1, 1)] += value
             else:
-                self.charges[date(current_date_time.year, current_date_time.month + 1, 1)] = value
+                self.charges[date(current_date_time.year, current_date_time.month+1, 1)] = value
         
-        self.power +=  power   
+        self.power += power
         return value    
-        
-#>>> pm1 = PowerMeter()
-#>>> pm1.meter(2)
-#Decimal('10.56')
-#>>> pm1.meter(1.2)
-#Decimal('6.34')
-#>>>
-#>>> pm1
-#<PowerMeter: 3.2 кВт/ч>
-#>>>
-#>>> print(pm1)
-#(August) 6.34
-#>>>        
+
+
+# >>> pm1 = PowerMeter()
+# >>> pm1.meter(2)
+# Decimal('10.56')
+# >>> pm1.meter(1.2)
+# Decimal('6.34')
+# >>>
+# >>> pm1
+# <PowerMeter: 3.2 кВт/ч>
+# >>>
+# >>> print(pm1)
+# (August) 6.34
+
--- 2023.08.10/4.py
+++ 2023.08.10/4.py
@@ -2,24 +2,24 @@ from pathlib import Path
 from sys import path
 import csv
 
+
 class CountableNouns:
-    """ Предоставляет интерфейс для работы с файловой базой существительных."""
-    
-    # атрибуты класса
+    """
+    Предоставляет интерфейс для работы с файловой базой существительных.
+    """
     db_path: Path = Path(r'words.csv')
     words: dict[str, tuple[str, str]] = {}
     
-    # Считывание данных из CSV файла
+    # считывание данных из CSV файла
     with open(db_path, encoding='utf-8') as r_file:
-        file_reader=csv.reader(r_file, delimiter=',')        
+        file_reader = csv.reader(r_file, delimiter=',')
         for row in file_reader:
             words[row[0]] = (row[1], row[2])
-    
-            
+
     @classmethod
-    def pick(cls, number: int, word: str)  -> str:
-        """Принимает в качестве аргументов число и существительное для согласования в единственном числе, возвращает согласованное с переданным числом существительное"""
-        number = number if number < 10 else number%10
+    def pick(cls, number: int, word: str) -> str:
+        """Принимает в качестве аргументов число и существительное для согласования в единственном числе, возвращает согласованное с переданным числом существительное."""
+        number = number if number < 10 else number % 10
         if number == 1:
             return word
         else: 
@@ -32,7 +32,7 @@ class CountableNouns:
                 cls.save_words(word)
         
     @classmethod
-    def save_words(cls, word1: str = None)  -> None:
+    def save_words(cls, word1: str = None) -> None:
         """Запрашивает в stdin у пользователя два или три слова согласующихся с числительными, добавляет полученные значения в поле класса words и дописывает их в файл с базой существительных"""
         if word1 is None:
             word1 = input('введите слово, согласующееся с числительным "один": ')
@@ -45,37 +45,38 @@ class CountableNouns:
             
         cls.words[word1] = (word2, word5)  
         
-        # Запись данных в CSV файл
+        # запись данных в CSV файл
         with open(cls.db_path, 'w', newline='', encoding='utf-8') as csvfile:
-            file_writer=csv.writer(csvfile, delimiter=',')
+            file_writer = csv.writer(csvfile, delimiter=',')
             for word in cls.words:
-                file_writer.writerow([word , cls.words[word][0], cls.words[word][1]])
+                file_writer.writerow([word, cls.words[word][0], cls.words[word][1]])
             
         return None    
-        
-#>>> CountableNouns.words
-#{'год': ('года', 'лет'), 'месяц': ('месяца', 'месяцев'), 'день': ('дня', 'дней')}
-#>>> CountableNouns.pick(22, 'год')
-#'года'
-#>>> CountableNouns.pick(365, 'день')
-#'дней'
-#>>> CountableNouns.pick(21, 'попугай')
-#'попугай'
-#>>> CountableNouns.pick(22, 'попугай')
-#существительное "попугай" отсутствует в базе
-#введите слово, согласующееся с числительным "два": попугая
-#введите слово, согласующееся с числительным "пять": попугаев
-#>>> CountableNouns.words
-#{'год': ('года', 'лет'), 'месяц': ('месяца', 'месяцев'), 'день': ('дня', 'дней'), 'попугай': ('попугая', 'попугаев')}
-#>>>
-#>>> CountableNouns.save_words()
-#введите слово, согласующееся с числительным "один": капля
-#введите слово, согласующееся с числительным "два": капли
-#введите слово, согласующееся с числительным "пять": капель
-#>>> print(CountableNouns.db_path.read_text(encoding='utf-8'))
-#год,года,лет
-#месяц,месяца,месяцев
-#день,дня,дней
-#попугай,попугая,попугаев
-#капля,капли,капель
-       
+
+
+# >>> CountableNouns.words
+# {'год': ('года', 'лет'), 'месяц': ('месяца', 'месяцев'), 'день': ('дня', 'дней')}
+# >>> CountableNouns.pick(22, 'год')
+# 'года'
+# >>> CountableNouns.pick(365, 'день')
+# 'дней'
+# >>> CountableNouns.pick(21, 'попугай')
+# 'попугай'
+# >>> CountableNouns.pick(22, 'попугай')
+# существительное "попугай" отсутствует в базе
+# введите слово, согласующееся с числительным "два": попугая
+# введите слово, согласующееся с числительным "пять": попугаев
+# >>> CountableNouns.words
+# {'год': ('года', 'лет'), 'месяц': ('месяца', 'месяцев'), 'день': ('дня', 'дней'), 'попугай': ('попугая', 'попугаев')}
+# >>>
+# >>> CountableNouns.save_words()
+# введите слово, согласующееся с числительным "один": капля
+# введите слово, согласующееся с числительным "два": капли
+# введите слово, согласующееся с числительным "пять": капель
+# >>> print(CountableNouns.db_path.read_text(encoding='utf-8'))
+# год,года,лет
+# месяц,месяца,месяцев
+# день,дня,дней
+# попугай,попугая,попугаев
+# капля,капли,капель
+
