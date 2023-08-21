e50477fbf50a79dc7a276e525578a42c605504fb
Sizov <ser-reb@yandex.ru>
2023-08-21 15:53:09 +0400

    устранение замечаний

--- 2023.08.10/2.py
+++ 2023.08.10/2.py
@@ -1,5 +1,5 @@
 # ДОБАВИТЬ: для типа Decimal есть общеупотребимый псевдоним dec
-from decimal import Decimal
+from decimal import Decimal as dec
 from datetime import time
 from datetime import date
 from datetime import datetime as dt
@@ -12,51 +12,43 @@ class PowerMeter:
     """
     # ИСПОЛЬЗОВАТЬ: в большинстве случаев аннотацию возвращаемых значений встроенных методов можно опустить
     # ИСПРАВИТЬ: согласно условию, конструктор должен предоставить возможность передать четыре аргумента
-    def __init__(self):
+    def __init__(self, tariff1: dec = 5.28, tariff2: dec = 2.5, tariff2_starts: time = time(23,00), tariff2_ends: time = time(7,00)):
         # ИСПРАВИТЬ: при использовании инструкции from ... import ... в глобальное пространство имён добавляется идентификатор импортированного объекта, а идентификатор модуля не добавляется — следовательно использовать имена decimal и datetime мы не можем
-        self.tariff1: decimal.Decimal = 5.28
-        self.tariff2: decimal.Decimal = 2.5
-        self.tariff2_starts: datetime.time = time(23,00)
-        self.tariff2_ends: datetime.time = time(7,00)
-        self.power: decimal.Decimal = 0
-        self.charges: dict[datetime.date, decimal.Decimal] = {}
+        self.tariff1 = dec(tariff1)
+        self.tariff2 = dec(tariff2)
+        self.tariff2_starts = tariff2_starts
+        self.tariff2_ends = tariff2_ends
+        self.power: dec = 0
+        self.charges: dict[date, dec] = {}
         
     def __repr__(self):
         return f'<{self.__class__.__name__}: {self.power} кВт/ч>'
     
     def __str__(self):
         today = date.today()
-        if today.month == 12:
+        try:
             # ИСПРАВИТЬ: а если ключа в словаре нет? (см. тест ниже)
             # ИСПРАВИТЬ: нас интересует текущий месяц, а не следующий
-            return f'({today:%B}) {self.charges[date(today.year+1, 1, 1)]}'  
-        else:
-            return f'({today:%B}) {self.charges[date(today.year, today.month+1, 1)]}'
+            return f'({today:%B}) {self.charges[date(today.year, today.month, 1)]}'  
+        except KeyError:
+            return 'Данные отсутствуют!'
         
-    def meter(self, power: Number) -> Decimal:
+    def meter(self, power: Number) -> dec:
         """Вычисляет и возвращает стоимость согласно тарифному плану, действующему в текущий момент."""
         current_date_time = dt.now()
         current_time = current_date_time.time()
 
         # если дневной тариф
         # ИСПОЛЬЗОВАТЬ: представьте числовую ось, поместите на ней метки tariff2_starts и tariff2_ends (вторая явно окажется правее), теперь нанесите на ось отметки current_time для истинного условия — в каком порядке у вас окажутся подписи меток, в таком же порядке записывайте неравенства — так станет куда проще воспринимать условие целиком
-        if current_time < self.tariff2_starts or self.tariff2_ends < current_time:
-            value = round(Decimal(power*self.tariff1), 2)
+        if self.tariff2_ends < current_time < self.tariff2_starts:
+            value = round(dec(power)*self.tariff1, 2)
         else:
-            value = round(Decimal(power*self.tariff2), 2)
+            value = round(dec(power)*self.tariff2, 2)
 
-        if current_date_time.month == 12:
-            # ИСПРАВИТЬ: используйте словарные методы get() и setdefault() вместо явной проверки наличия ключа
-            if date(current_date_time.year+1, 1, 1) in self.charges:
-                # ИСПРАВИТЬ: нас интересует текущий месяц, а не следующий
-                self.charges[date(current_date_time.year+1, 1, 1)] += value
-            else:
-                self.charges[date(current_date_time.year+1, 1, 1)] = value
-        else:
-            if date(current_date_time.year+1, 1, 1) in self.charges:
-                self.charges[date(current_date_time.year, current_date_time.month+1, 1)] += value
-            else:
-                self.charges[date(current_date_time.year, current_date_time.month+1, 1)] = value
+        # ИСПРАВИТЬ: используйте словарные методы get() и setdefault() вместо явной проверки наличия ключа
+        # ИСПРАВИТЬ: нас интересует текущий месяц, а не следующий                
+        self.charges.setdefault(date(current_date_time.year, current_date_time.month, 1), 0)
+        self.charges[date(current_date_time.year, current_date_time.month, 1)] += value
 
         self.power += power
         return value
@@ -83,3 +75,19 @@ class PowerMeter:
 
 
 # ИТОГ: надо лучше, доработать — 4/7
+
+# >>> pm1 = PowerMeter()
+# >>> pm1.meter(2)
+# Decimal('10.56')
+# >>> pm1.meter(1.2)
+# Decimal('6.34')
+# >>>
+# >>> pm1
+# <PowerMeter: 3.2 кВт/ч>
+# >>>
+# >>> print(pm1)
+# (August) 16.90
+# >>>
+# >>> pm1 = PowerMeter()
+# >>> print(pm1)
+# Данные отсутствуют!
